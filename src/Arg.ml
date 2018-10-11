93f316287ca301f1ea209878eef6c89bfee9a960
WhoAteDaCake <doitsimplefy@gmail.com>
2018-10-11 23:16:26 +0100

    add command line parsing

--- src/Arg.ml
+++ src/Arg.ml
@@ -1,10 +1,87 @@
 (* Idea *)
 
+exception No_arguments of string
+
 type spec =
- | Bool of (bool -> unit)
- | List of (string list -> unit)
- | String of (string -> unit)
+ | Single of (bool -> unit)
+ | Pair of (string -> unit)
+ | Entries of (string list -> unit)
+
+type flag =
+  | Single_Flag of bool option
+  | Pair_Flag of string option
+  | Entries_Flag of (string list) option
+
+type config = string * spec * string
 
 let args () = Array.to_list (Utils.from_nth 2 Sys.argv)
 
-(* let parse = specs *)
+let rec find_flags key flag args = match args with
+  | [] -> flag
+  | hd :: [] ->
+    if not (hd == key) then
+      flag
+    else
+      let result = match flag with
+        | Single_Flag(_) -> Single_Flag (Some true)
+        | _ -> flag in
+      result
+  | hd :: value :: rest ->
+    if not (hd == key) then
+      find_flags key flag (value :: rest)
+    else match flag with
+      | Single_Flag(_) -> Single_Flag (Some true)
+      | Pair_Flag(_) -> Pair_Flag (Some value)
+      | Entries_Flag(ls_opt) ->
+        let new_entries = match ls_opt with
+          | None -> [value]
+          | Some(ls) -> value :: ls in
+        (* If we need to maintain order acc @ [value] should be used or list should be reversed after *)
+        find_flags key (Entries_Flag (Some new_entries)) rest      
+
+
+let handle_spec args config = 
+  let (key, spec, _) = config in
+  match spec with
+    | Single(fn) ->
+      (match find_flags key (Single_Flag None) args with
+        | Single_Flag(Some(value)) -> fn(value)
+        | _ -> ()
+      )
+    | Pair(fn) ->
+      (match find_flags key (Pair_Flag None) args with
+        | Pair_Flag(Some(value)) -> fn(value)
+        | _ -> ()
+      )
+    | Entries(fn) -> 
+      (match find_flags key (Entries_Flag None) args with
+        | Entries_Flag(Some(value)) -> fn(value)
+        | _ -> ()
+      )
+
+let parse specs args = List.map (handle_spec args) specs
+
+let format_specs specs spaces = List.map
+  (fun spec -> 
+     let (key, _, message) = spec in
+     (key, (String.make spaces ' ') ^ message)
+  ) specs
+
+(* TODO implement a check whether the command actually exists in spec *)
+let handle ?(spaces=4) ~when_anon specs args =
+  if List.length args == 1 then
+    match List.hd args with
+      | "help" | "--help" -> 
+        let messages = format_specs specs spaces in
+        List.map (fun (key, message) -> 
+          print_endline(key);
+          print_endline(message)
+        ) messages |> ignore
+      | str -> (
+        if String.get str 0 == '-' then
+          parse specs args |> ignore
+        else 
+          when_anon str
+      )
+  else
+    ignore (parse specs args )
--- src/main.ml
+++ src/main.ml
@@ -1,4 +1,11 @@
+
 let main () = 
-	Arg.args () |> List.map (print_endline)
+	let includes = ref [] in
+	let base_route = ref (Sys.getcwd ()) in
+	let specs = [
+		("--i", Arg.Entries(fun items -> includes := items), "Include items");
+		("--a", Arg.Entries(fun items -> includes := items), "Include a items")
+	] in
+	Arg.handle ~when_anon:(print_endline) specs (Arg.args ())
 
 let _ = main ()
