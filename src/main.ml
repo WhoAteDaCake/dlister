c1ac8fa6363010b9596aae8812fc332876dc98db
WhoAteDaCake <doitsimplefy@gmail.com>
2018-10-12 09:28:28 +0100

    add vendors

--- src/main.ml
+++ src/main.ml
@@ -2,11 +2,16 @@
 let main () = 
 	let includes = ref [] in
 	let base_route = ref (Sys.getcwd ()) in
+	let route = Fpath.v !base_route in
 	let specs = [
 		("--i", Arg.Entries(fun items -> includes := items), "Include items");
 		("--a", Arg.Entries(fun items -> includes := items), "Include a items")
 	] in
-	let continue = Arg.handle ~when_anon:(print_endline) specs (Arg.args ()) in
+	let continue = Arg.handle
+		~when_anon:(fun path ->
+			base_route := Filename.realpath (Filename.concat !base_route path) )
+		specs
+		(Arg.args ()) in
 	if not continue then 
 		()
 	else 
