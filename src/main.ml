149fd03991ed5607293155e4a1f097d1318e79dc
WhoAteDaCake <doitsimplefy@gmail.com>
2018-10-12 09:40:01 +0100

    fix build

--- src/main.ml
+++ src/main.ml
@@ -1,20 +1,19 @@
 
 let main () = 
 	let includes = ref [] in
-	let base_route = ref (Sys.getcwd ()) in
-	let route = Fpath.v !base_route in
+	let base_route = ref (Fpath.v (Sys.getcwd ())) in
 	let specs = [
 		("--i", Arg.Entries(fun items -> includes := items), "Include items");
 		("--a", Arg.Entries(fun items -> includes := items), "Include a items")
 	] in
 	let continue = Arg.handle
 		~when_anon:(fun path ->
-			base_route := Filename.realpath (Filename.concat !base_route path) )
+			base_route := Fpath.append !base_route (Fpath.v path) )
 		specs
 		(Arg.args ()) in
 	if not continue then 
 		()
 	else 
-		print_endline !base_route
+		print_endline (Fpath.to_string !base_route)
 
 let _ = main ()
