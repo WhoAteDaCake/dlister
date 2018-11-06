fcfd01a3b0c9078395f4d84cfd6c54904a231bd9
WhoAteDaCake <doitsimplefy@gmail.com>
2018-11-06 22:04:55 +0000

    add is_dir function from node bindings

--- src/file.ml
+++ src/file.ml
@@ -1,3 +1,15 @@
+
+type stats = <
+  isDirectory: unit -> bool [@bs.meth]
+> Js.t
+
+external stats_sync: string -> stats  = "statSync" [@@bs.val] [@@bs.module "fs"]
+
+let is_dir path =
+  let stats = stats_sync path in  
+  stats##isDirectory ()
+
+
 let read_file file =
   let open Node_fs in
   readFileSync file `utf8
@@ -8,3 +20,5 @@ let exists = Node_fs.existsSync
 let write_file file contents =
   let open Node_fs in
   writeFileSync file contents `utf8 
+
+let read_dir dir = Node_fs.readdirSync dir |> Belt.List.fromArray
