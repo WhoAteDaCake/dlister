b718b1b5b87025fbd2514d40a66fcdc4958eac5f
WhoAteDaCake <doitsimplefy@gmail.com>
2018-11-25 20:58:51 +0000

    use terser minifier

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
 const nodeExternals = require("webpack-node-externals");
-const mode = process.env.NODE_ENV || "development";
-const pkg = require('./package.json');
+const TerserPlugin = require("terser-webpack-plugin");
+const pkg = require("./package.json");
 
 module.exports = {
   target: "node",
+  mode: "production",
   entry: pkg.source,
   externals: [
     nodeExternals({
@@ -16,9 +17,12 @@ module.exports = {
     path: path.resolve(__dirname, "dist")
   },
   optimization: {
-		// We no not want to minimize our code.
-    minimize: false,
-    usedExports: true
-	},
-  mode
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          mangle: false
+        }
+      })
+    ]
+  }
 };
